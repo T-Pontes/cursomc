ca41ca26efb49123324a11f4bd0980b46a178c34
T-Pontes <tiagopontes10@gmail.com>
2022-01-06 20:58:30 -0300

    Estado e cidade

---
+++ src/main/java/com/tiagopontes/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.tiagopontes.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.tiagopontes.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
