7330876e80b381a6d2759e7fbe614ae70d4de24a
T-Pontes <tiagopontes10@gmail.com>
2022-01-05 22:59:15 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/tiagopontes/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.tiagopontes.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.tiagopontes.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
