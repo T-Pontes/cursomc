fb9b29cf188f9ab3c14319eca39bb39f2e214d2f
T-Pontes <tiagopontes10@gmail.com>
2022-01-08 11:26:58 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/tiagopontes/cursomc/CursomcApplication.java
+++ src/main/java/com/tiagopontes/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.tiagopontes.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.tiagopontes.cursomc.domain.Cidade;
 import com.tiagopontes.cursomc.domain.Cliente;
 import com.tiagopontes.cursomc.domain.Endereco;
 import com.tiagopontes.cursomc.domain.Estado;
+import com.tiagopontes.cursomc.domain.Pagamento;
+import com.tiagopontes.cursomc.domain.PagamentoComBoleto;
+import com.tiagopontes.cursomc.domain.PagamentoComCartao;
+import com.tiagopontes.cursomc.domain.Pedido;
 import com.tiagopontes.cursomc.domain.Produto;
+import com.tiagopontes.cursomc.domain.enums.EstadoPagamento;
 import com.tiagopontes.cursomc.domain.enums.TipoCliente;
 import com.tiagopontes.cursomc.repositories.CategoriaRepository;
 import com.tiagopontes.cursomc.repositories.CidadeRepository;
 import com.tiagopontes.cursomc.repositories.ClienteRepository;
 import com.tiagopontes.cursomc.repositories.EnderecoRepository;
 import com.tiagopontes.cursomc.repositories.EstadoRepository;
+import com.tiagopontes.cursomc.repositories.PagamentoRepository;
+import com.tiagopontes.cursomc.repositories.PedidoRepository;
 import com.tiagopontes.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -84,6 +96,19 @@ public class CursomcApplication implements CommandLineRunner{
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 		
+		SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1,e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null,EstadoPagamento.QUITADO,ped1,6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2 = new PagamentoComBoleto(null,EstadoPagamento.PENDENTE,ped2,sdf.parse("20/10/2017 00:00"),null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 	}
 
 }
