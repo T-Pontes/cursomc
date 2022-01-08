ec9916fdb7c988f41ca3206bd332605bffceb64d
T-Pontes <tiagopontes10@gmail.com>
2022-01-08 13:06:14 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/tiagopontes/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package com.tiagopontes.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.tiagopontes.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
